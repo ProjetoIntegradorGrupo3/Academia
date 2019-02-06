2b1004b977c1749249218c3e808d6191d4ed5789
Mateus Da Silva Avila <181720083@senacinformatica.br>
2019-02-06 21:32:11 -0200

    ajuste de local do projeto

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -4,8 +4,51 @@ package model;
 
 public class Aluno {
     
-    private String nome, cpf, email;
-    private int  numero; 
+    private String nome, cpf, email, numero;
+    private int  codigo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+    
     
     
     
